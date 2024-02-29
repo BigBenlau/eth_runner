8977fc92b536e3955f830d081b7bcfcfe65edf7a
Ben Lao <benlau413@gmail.com>
2024-02-29 18:40:07 +0800

    update main.rs read block_num and gas_used list from csv

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,17 @@ use reth_revm::{
 
 use std::path::Path;
 use std::sync::Arc;
-use rand::distributions::{Distribution, Uniform};
 use chrono::Local;
+
+use std::time::Duration;
+use std::fs::File;
+use csv::{Reader, Error};
 
 // #[derive(Parser, Debug)]
 
-fn main() {
-    let start_time = Local::now();
-    println!("Start Current Time is {:?}", start_time);
 
+fn main() -> Result<(), Error> {
+    // Read Database Info
     let string = String::from("/home/user/common/docker/volumes/eth-docker_reth-el-data/_data/db");
     let path = Path::new(&string);
     let db = Arc::new(open_db_read_only(&path, None).unwrap());
@@ -31,17 +33,23 @@ fn main() {
 
     let provider = Arc::new(ProviderFactory::new(&db, chain_spec.clone()));
 
-    let loop_time = 100;
 
-    for i in 0..loop_time {
-        // let block_range = 18650000..18768100;
+    let start_time = Local::now();
+    println!("Start Current Time is {:?}", start_time);
 
-        // let mut rng = rand::thread_rng();
-        // let block_uniform_range = Uniform::from(block_range);
+    // Execute Block by block number
+    let mut round_num = 0;
+    let mut gas_used_sum = 0;
+    let mut exec_time_sum = Duration::new(0, 0);
+    let file = File::open("block_range.csv")?;
+    let mut reader = Reader::from_reader(file);
 
-        // let old_block_num= block_uniform_range.sample(&mut rng);
-        let old_block_num = 18768099;
-        let new_block_num = old_block_num + 1;
+    for result in reader.records() {
+        let record = result?;
+        let new_block_num = record[0].parse::<u64>().unwrap();
+        let gas_used = record[1].parse::<u128>().unwrap();
+
+        let old_block_num = new_block_num - 1;
         let new_block = provider.block(BlockHashOrNumber::Number(new_block_num)).unwrap().unwrap();
 
 
@@ -51,16 +59,26 @@ fn main() {
 
         // let result = executor.execute_and_verify_receipt(&new_block, U256::ZERO, None).unwrap();
 
-        let result = executor.execute(&new_block, U256::ZERO, None).unwrap();
+        executor.execute(&new_block, U256::ZERO, None).unwrap();
 
         let stat = executor.stats();
         println!("Show stats: {:?}", stat);
+        let exec_time = stat.execution_duration;
 
-        println!("new block number {:?}, round: {:?}", new_block_num, i);
+        round_num += 1;
+        gas_used_sum += gas_used;
+        exec_time_sum += exec_time;
+        println!("new block number {:?}, round: {:?}", new_block_num, round_num);
     }
+
+
     let end_time = Local::now();
     println!("End Current Time is {:?}", end_time);
 
     let diff = end_time - start_time;
-    println!("Duration Time is {:?} ms", diff.num_milliseconds());
+    println!("Duration Time is {:?} ms\n", diff.num_milliseconds());
+
+    let gas_per_ms = gas_used_sum / exec_time_sum.as_millis();
+    println!("Total Gas Used is {:?} \nTotal Execution Time is {:?}\n Gas Used per millisecond is {:?}", gas_used_sum, exec_time_sum, gas_per_ms);
+    Ok(())
 }
