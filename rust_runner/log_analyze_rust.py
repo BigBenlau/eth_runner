851bb9de45954e108536cffce140550a89198e54
Ben Lao <benlau413@gmail.com>
2024-06-13 00:50:10 +0800

    rust_runner log_analyse script rename

--- rust_runner/log_analyze_rust.py
+++ rust_runner/log_analyze_rust.py
@@ -6,6 +6,8 @@ output_filename = "opcode_average_time.txt"
 output_file = open(output_filename, "w")
 
 count_dict = {}
+total_opcode_time = 0
+total_opcode_time_except_call = 0
 
 while True:
     line = read_file.readline()
@@ -24,6 +26,13 @@ while True:
 
         count_dict[opcode]["count"] += 1
         count_dict[opcode]["total_time"] += dur_time
+        total_opcode_time += dur_time
+        if opcode not in ["DELEGATECALL", "CALL", "STATICCALL"]:
+            total_opcode_time_except_call += dur_time
+
+print("scan file finished!!")
+print("total opcode time used: %s" % round(total_opcode_time / 10**9, 4))
+print("total opcode time except call: %s" % round(total_opcode_time_except_call / 10**9, 4))
 
 for key in count_dict:
     count_dict_item = count_dict[key]
@@ -31,5 +40,6 @@ for key in count_dict:
 
 sorted_list = sorted(count_dict.items(), key=lambda item:item[1]["avg_time"], reverse=True)
 
+output_file.write("Opcode\tAverage Time\tCount\n")
 for item in sorted_list:
-    output_file.write("%s\t%s\n" % (item[0], item[1]["avg_time"]))
+    output_file.write("%s\t%s\t%s\n" % (item[0], item[1]["avg_time"], item[1]["count"]))
